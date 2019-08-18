4e47448eb446bbf7d400285239bfb3f72e797455
kaitoyagi <51041115+kaitoyagi@users.noreply.github.com>
2019-08-18 16:58:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ foreach ($post_data as $posts_data){
       <div class="post_data">
           <p class="name">出題者：<?php  echo $posts_data['name']; ?></p>
           <h3>お題：<?php  echo $posts_data['post_content']; ?></h3>
-          <a href="comment.php?id=<?php echo $posts_data['post_id']; ?> ">&raquo; ボケる</a>
+          <a href="comment.php?id=<?php echo $posts_data['post_id']; ?> ">&raquo; 回答</a>
+          <a href="detail.php?id=<?php echo $posts_data['post_id']; ?> ">&raquo; ボケる</a>
       </div>
     </body>
 <?php } ?>
